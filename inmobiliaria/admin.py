3e058807cf907143a7095b49e5f0ddd1fc2f1c54
fransalva16 <fran65533@gmail.com>
2025-01-06 10:29:40 -0300

    add

--- inmobiliaria/admin.py
+++ inmobiliaria/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Vendedor, Inquilino, Propietario
+from .models import Vendedor, Inquilino, Propietario, Movimiento
 
 @admin.register(Vendedor)
 class VendedorAdmin(admin.ModelAdmin):
@@ -16,3 +16,10 @@ class PropietarioAdmin(admin.ModelAdmin):
     list_display = ('dni', 'nombre', 'apellido', 'email', 'celular', 'cuenta_bancaria')
     search_fields = ('dni', 'nombre', 'apellido', 'email')
     list_filter = ('fecha_nacimiento',)
+
+@admin.register(Movimiento)
+class MovimientoAdmin(admin.ModelAdmin):
+    list_display = ['fecha', 'tipo', 'categoria', 'monto', 'usuario', 'sucursal']
+    list_filter = ['tipo', 'categoria', 'fecha', 'sucursal']
+    search_fields = ['descripcion']
+    date_hierarchy = 'fecha'
