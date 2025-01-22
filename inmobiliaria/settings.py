d426020dc709e995f27f896686cbae681330e3d4
fransalva16 <fran65533@gmail.com>
2025-01-22 09:40:51 -0300

    add

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -1,4 +1,22 @@
+import os
+
 # Configuración de sesión
 SESSION_COOKIE_AGE = 600  # 10 minutos en segundos
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Configuración para servir archivos estáticos en producción
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MIDDLEWARE = [
+    # ...
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Agregar después de SecurityMiddleware
+    # ... resto de middleware
+]
