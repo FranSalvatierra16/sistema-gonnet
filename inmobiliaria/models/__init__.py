2bb7b3ebb9bc24778d52d482eb8c59416989fb39
fransalva16 <fran65533@gmail.com>
2025-02-26 09:38:07 -0300

    add

--- inmobiliaria/models/__init__.py
+++ inmobiliaria/models/__init__.py
@@ -1,4 +1,4 @@
 from .persona import Vendedor, Inquilino, Propietario
 from .propiedad import Propiedad,  Reserva, Disponibilidad, ImagenPropiedad,Precio, TipoPrecio,TIPOS_INMUEBLES, TIPOS_VISTA, TIPOS_VALORACION, ConceptoPago, Pago, HistorialDisponibilidad, VentaPropiedad, AlquilerMeses   
 from .sucursal import Sucursal
-from .caja import Caja, MovimientoCaja, ValePersonal, ConceptoPago, Pago, HistorialDisponibilidad, VentaPropiedad, AlquilerMeses   
+from .caja import Caja, MovimientoCaja, ValePersonal, ConceptoPago, ComisionVenta, PagoAlquiler
