9da9d8cca2ca8cf361f4cb7b22ba6a5154d27003
fransalva16 <fran65533@gmail.com>
2025-05-07 10:26:10 -0300

    add

--- inmobiliaria/urls.py
+++ inmobiliaria/urls.py
@@ -122,9 +122,9 @@ urlpatterns = [
     path('caja/nuevo-registro/', views.nuevo_registro, name='nuevo_registro'),
     path('caja/nuevo-concepto/', views.nuevo_concepto, name='nuevo_concepto'),
     path('caja/buscar-cuentas/', views.buscar_cuentas, name='buscar_cuentas'),
-    path('reserva/buscar-propiedades/', views.buscar_propiedades, name='buscar_propiedades'),
+ 
     path('crear-concepto/', views.crear_concepto, name='crear_concepto'),
     path('crear-propiedad/', views.crear_propiedad, name='crear_propiedad'),
-    path('buscar-propiedades/', views.buscar_propiedades, name='buscar_propiedades'),
+
     path('api/propiedades/select2/', views.buscar_propiedades_select2, name='buscar_propiedades_select2'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
