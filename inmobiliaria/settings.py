4363aae665992f3025d9d36801a064e96ccdfa8d
fransalva16 <fran65533@gmail.com>
2025-05-20 12:03:35 -0300

    add

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -51,12 +51,7 @@ INSTALLED_APPS = [
 
 FILE_UPLOAD_MAX_MEMORY_SIZE = 26214400  # 25 MB
 
-# Configuración para Amazon S3
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
