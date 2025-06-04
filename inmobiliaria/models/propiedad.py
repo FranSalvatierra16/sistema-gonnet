91724a98a9b3bde01e83d09a71a76b85f96cd4df
fransalva16 <fran65533@gmail.com>
2025-06-04 12:48:54 -0300

    prueba que fallo

--- inmobiliaria/models/propiedad.py
+++ inmobiliaria/models/propiedad.py
@@ -810,7 +810,3 @@ def reordenar_numeros_por_propietario(sender, instance, **kwargs):
         if propiedad.numero_por_propietario != idx:
             propiedad.numero_por_propietario = idx
             propiedad.save(update_fields=['numero_por_propietario'])
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
