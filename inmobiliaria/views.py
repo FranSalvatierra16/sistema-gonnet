52bed821848d23138c6cb9ed6e55b10a9c5691af
fransalva16 <fran65533@gmail.com>
2025-07-04 10:31:46 -0300

    en el anterior m,e dejo de andar

--- inmobiliaria/views.py
+++ inmobiliaria/views.py
@@ -649,15 +649,15 @@ def confirmar_reserva(request):
                 # Obtener objetos necesarios
                 propiedad = get_object_or_404(Propiedad, id=propiedad_id)
                 vendedor = get_object_or_404(Vendedor, id=vendedor_id)
-                inquilino = get_object_or_404(Inquilino, id=inquilino_id)
+                cliente = get_object_or_404(Inquilino, id=inquilino_id)  # Obtenemos el inquilino
 
-                # Crear la reserva
+                # Crear la reserva usando 'cliente' en lugar de 'inquilino'
                 reserva = Reserva.objects.create(
                     propiedad=propiedad,
                     fecha_inicio=fecha_inicio,
                     fecha_fin=fecha_fin,
                     vendedor=vendedor,
-                    inquilino=inquilino,
+                    cliente=cliente,  # Cambiado de 'inquilino' a 'cliente'
                     precio_total=float(precio.replace(',', ''))
                 )
 
