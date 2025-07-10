43fac87c5a5c3d732ba3f09dea19562b093b36bf
fransalva16 <fran65533@gmail.com>
2025-07-10 10:16:12 -0300

    agregando config

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -45,6 +45,7 @@ DEBUG = False  # Asegúrate de que esté en False en producción
 INSTALLED_APPS = [
     # ... otras apps ...
     'django.contrib.humanize',
+    'storages',
     # ... resto de las apps ...
 ]
 
@@ -52,3 +53,12 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 26214400  # 25 MB
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if not DEBUG:  # En producción (Heroku)
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = 'us-east-1'  # Cambiado a tu región
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
