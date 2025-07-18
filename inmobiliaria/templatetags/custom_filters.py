eb9010ab00696fc5c37d898af46284de3f48862d
fransalva16 <fran65533@gmail.com>
2025-07-18 12:56:08 -0300

    ingreso positivo

--- inmobiliaria/templatetags/custom_filters.py
+++ inmobiliaria/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from decimal import Decimal
 
 register = template.Library()
 
@@ -7,4 +8,12 @@ def format_price(value):
     try:
         return "{:,.0f}".format(value).replace(',', '.')
     except (ValueError, TypeError):
+        return value
+
+@register.filter
+def abs(value):
+    """Returns the absolute value of a number"""
+    try:
+        return abs(Decimal(str(value)))
+    except (TypeError, ValueError):
         return value
