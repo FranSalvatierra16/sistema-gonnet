ec0b2549bab86ac12e690f1f654f90c5b71e64fd
fransalva16 <fran65533@gmail.com>
2025-08-07 09:31:17 -0300

    add

--- inmobiliaria/templatetags/custom_filters.py
+++ inmobiliaria/templatetags/custom_filters.py
@@ -47,3 +47,50 @@ def sub(value, arg):
         return float(value) - float(arg)
     except (ValueError, TypeError):
         return 0
+
+@register.filter
+def get_caracteristicas(propiedad):
+    """Genera una lista de características de la propiedad basada en sus campos booleanos"""
+    caracteristicas = []
+    
+    if propiedad.amoblado:
+        caracteristicas.append('Amoblado')
+    if propiedad.cochera:
+        caracteristicas.append('Cochera')
+    if propiedad.tv_smart:
+        caracteristicas.append('TV Smart')
+    if propiedad.wifi:
+        caracteristicas.append('WiFi')
+    if propiedad.dependencia:
+        caracteristicas.append('Dependencia')
+    if propiedad.patio:
+        caracteristicas.append('Patio')
+    if propiedad.parrilla:
+        caracteristicas.append('Parrilla')
+    if propiedad.piscina:
+        caracteristicas.append('Piscina')
+    if propiedad.reciclado:
+        caracteristicas.append('Reciclado')
+    if propiedad.a_estrenar:
+        caracteristicas.append('A estrenar')
+    if propiedad.terraza:
+        caracteristicas.append('Terraza')
+    if propiedad.balcon:
+        caracteristicas.append('Balcón')
+    if propiedad.baulera:
+        caracteristicas.append('Baulera')
+    if propiedad.lavadero:
+        caracteristicas.append('Lavadero')
+    if propiedad.seguridad:
+        caracteristicas.append('Seguridad')
+    if propiedad.vista_al_Mar:
+        caracteristicas.append('Vista al Mar')
+    if propiedad.vista_panoramica:
+        caracteristicas.append('Vista Panorámica')
+    if propiedad.apto_credito:
+        caracteristicas.append('Apto Crédito')
+    
+    if not caracteristicas:
+        return 'Sin características especiales'
+    
+    return ', '.join(caracteristicas)
