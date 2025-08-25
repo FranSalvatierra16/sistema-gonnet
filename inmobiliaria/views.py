3f601e6ffb12c477dec5c4f57773dcf61551e69c
fransalva16 <fran65533@gmail.com>
2025-08-25 12:17:02 -0300

    add

--- inmobiliaria/views.py
+++ inmobiliaria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, JsonResponse
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.utils import timezone
