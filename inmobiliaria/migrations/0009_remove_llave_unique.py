1330469a5638e6a351b6c4159c8099e486726211
fransalva16 <fran65533@gmail.com>
2025-11-06 00:32:48 -0300

    Fix: Migración 0009 compatible con PostgreSQL
    - Cambiar DROP INDEX (MySQL) a DROP CONSTRAINT (PostgreSQL)
    - Usar RunPython con detección de vendor
    - Resolver: syntax error at or near 'llave'
    - Permitir migraciones en PostgreSQL (Railway)

--- inmobiliaria/migrations/0009_remove_llave_unique.py
+++ inmobiliaria/migrations/0009_remove_llave_unique.py
@@ -1,5 +1,32 @@
 from django.db import migrations
 
+def remove_llave_index(apps, schema_editor):
+    """
+    Eliminar índice 'llave' de forma compatible con MySQL y PostgreSQL
+    """
+    if schema_editor.connection.vendor == 'mysql':
+        schema_editor.execute('ALTER TABLE inmobiliaria_propiedad DROP INDEX llave;')
+    elif schema_editor.connection.vendor == 'postgresql':
+        # En PostgreSQL, primero verificar si existe la constraint
+        schema_editor.execute("""
+            DO $$ 
+            BEGIN
+                IF EXISTS (
+                    SELECT 1 FROM pg_constraint 
+                    WHERE conname = 'llave' 
+                    AND conrelid = 'inmobiliaria_propiedad'::regclass
+                ) THEN
+                    ALTER TABLE inmobiliaria_propiedad DROP CONSTRAINT llave;
+                END IF;
+            END $$;
+        """)
+
+def add_llave_index(apps, schema_editor):
+    """
+    Restaurar índice 'llave' (reverse operation)
+    """
+    schema_editor.execute('ALTER TABLE inmobiliaria_propiedad ADD UNIQUE (llave);')
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -7,8 +34,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql='ALTER TABLE inmobiliaria_propiedad DROP INDEX llave;',
-            reverse_sql='ALTER TABLE inmobiliaria_propiedad ADD UNIQUE (llave);'
-        ),
+        migrations.RunPython(remove_llave_index, reverse_code=add_llave_index),
     ] 
