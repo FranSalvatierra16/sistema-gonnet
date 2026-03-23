8ddc2a02a3513c5435fbca9f0aeb1b42baa24b09
fransalva16 <fran65533@gmail.com>
2026-03-23 11:39:34 -0300

    add

---
+++ inmobiliaria/migrations/0091_merge_0090_branches.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inmobiliaria', '0090_ampliar_decimal_caja_movimientos'),
+        ('inmobiliaria', '0090_liquidacion_estados_nuevos'),
+    ]
+
+    operations = []
+
